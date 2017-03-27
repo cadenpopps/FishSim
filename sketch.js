e84c9e8628f188a6de5e760d36e051034a18e8af
Caden Popps <cadenpopps@gmail.com>
2017-03-27 13:49:25 -0500

    UI Updates RELEASE READY 3/27

--- sketch.js
+++ sketch.js
@@ -15,138 +15,149 @@ var fishMoveSpeedSlider;
 var fishSpeedLimitSlider;
 var fishDensitySlider;
 var schoolPopulation;
-const sliderDistance = 8;
+const sliderDistance = 7;
 
 function preload() {
-   //font = loadFont('data/SourceCodePro-Light.otf');
+    //font = loadFont('data/SourceCodePro-Light.otf');
 }
 
 function setup() {
 
-   createCanvas(windowWidth, windowHeight);
-   //textFont(font);
-   textAlign(CENTER);
+    createCanvas(windowWidth, windowHeight);
+    //textFont(font);
+    textAlign(CENTER);
 
-   schoolLimitSlider = createSlider(1, 30, 10);
-   schoolLimitSlider.position(20, 1 * (height / sliderDistance));
+    schoolLimitSlider = createSlider(0, 30, 10);
+    //schoolLimitSlider.position(1 * (width / sliderDistance) - 100, height - 50);
 
-   schoolMoveSpeedSlider = createSlider(0, 100, 20);
-   schoolMoveSpeedSlider.position(20, 2 * (height / sliderDistance));
+    schoolMoveSpeedSlider = createSlider(0, 100, 20);
+    //schoolMoveSpeedSlider.position(2 * (width / sliderDistance) - 100, height - 50);
 
-   fishMoveSpeedSlider = createSlider(0, 100, 20);
-   fishMoveSpeedSlider.position(20, 3 * (height / sliderDistance));
+    fishMoveSpeedSlider = createSlider(0, 100, 20);
+    //fishMoveSpeedSlider.position(3 * (width / sliderDistance) - 100, height - 50);
 
-   fishSpeedLimitSlider = createSlider(1, 50, 20);
-   fishSpeedLimitSlider.position(20, 4 * (height / sliderDistance));
+    fishSpeedLimitSlider = createSlider(0, 50, 20);
+    //fishSpeedLimitSlider.position(4 * (width / sliderDistance) - 100, height - 50);
 
-   fishDensitySlider = createSlider(1, 100, 30);
-   fishDensitySlider.position(20, 5 * (height / sliderDistance));
+    fishDensitySlider = createSlider(0, 100, 30);
+    //fishDensitySlider.position(5 * (width / sliderDistance) - 100, height - 50);
 
-   schoolPopulation = createSlider(1, 100, 30);
-   schoolPopulation.position(20, 6 * (height / sliderDistance));
+    schoolPopulation = createSlider(1, 100, 30);
+    //schoolPopulation.position(6 * (width / sliderDistance) - 100, height - 50);
 
-   noStroke();
-   makeOcean();
+    noStroke();
+    makeOcean();
 
 }
 
 function draw() {
 
-   background(255, 50);
+    background(255, 50);
 
-   if (ocean.length > SCHOOLLIMIT) {
-      ocean.splice(ocean.length - 1);
-   }
+    if (ocean.length > SCHOOLLIMIT) {
+        ocean.splice(ocean.length - 1);
+    }
 
-   if (millis() < 3000) {
-      fill(0);
-      textSize(200);
-      textAlign(CENTER);
-      text("Fish", width/5, height / 2 - 100, width, height);
-   }
+    if (millis() < 2000) {
+        fill(20, 150);
+        textSize(200);
+        textAlign(CENTER);
+        text("Fish", 0, height / 2 - 150, width, height);
+    }
+    else if (millis() < 2500) {
+        fill(20, map(millis(), 2000, 2500, 150, 0));
+        textSize(200);
+        textAlign(CENTER);
+        text("Fish", 0, height / 2 - 150, width, height);
+    }
 
-   //sometimes add fish
-   if (random(1) < .01 && ocean.length < SCHOOLLIMIT) {
-      addSchool();
-   }
+    //sometimes add fish
+    if (random(1) < .01 && ocean.length < SCHOOLLIMIT) {
+        addSchool();
+    }
 
-   for (let s of ocean) {
-      s.update();
-      s.display();
-   }
+    for (let s of ocean) {
+        s.update();
+        s.display();
+    }
 
-   for (let f of foods) {
-      f.update();
-      f.display();
-   }
-   // shark.update();
-   // shark.display();
+    for (let f of foods) {
+        f.update();
+        f.display();
+    }
+    // shark.update();
+    // shark.display();
 
-   fill(50);
-   rect(0, 0, width / 5, height);
+    drawUI();
 
-   textAlign(LEFT);
-   textSize(width/80);
-   fill(255);
-   SCHOOLLIMIT = schoolLimitSlider.value();
-   text("School limit: " + SCHOOLLIMIT, 20, 1.5 * (height / sliderDistance));
-   schoolLimitSlider.position(20, 1 * (height / sliderDistance));
+}
 
-   SCHOOLMOVESPEED = schoolMoveSpeedSlider.value() / 100;
-   text("School move frequency: " + SCHOOLMOVESPEED, 20, 2.5 * (height / sliderDistance));
-   schoolMoveSpeedSlider.position(20, 2 * (height / sliderDistance));
+function drawUI() {
 
-   FISHMOVESPEED = fishMoveSpeedSlider.value() / 100;
-   text("Fish move frequency " + FISHMOVESPEED, 20, 3.5 * (height / sliderDistance));
-   fishMoveSpeedSlider.position(20, 3 * (height / sliderDistance));
+    fill(100, 150);
+    rect(100, height - 90, width - 200, 70);
 
-   FISHSPEEDLIMIT = fishSpeedLimitSlider.value();
-   text("Fish speed: " + FISHSPEEDLIMIT, 20, 4.5 * (height / sliderDistance));
-   fishSpeedLimitSlider.position(20, 4 * (height / sliderDistance));
+    textAlign(CENTER);
+    textSize(width / 100);
+    fill(255);
+    SCHOOLLIMIT = schoolLimitSlider.value();
+    text("School limit", 1 * (width / sliderDistance) - 10, height - 65);
+    schoolLimitSlider.position(1 * (width / sliderDistance) - 75, height - 60);
 
-   FISHDENSITY = fishDensitySlider.value();
-   text("Fish density: " + FISHDENSITY, 20, 5.5 * (height / sliderDistance));
-   fishDensitySlider.position(20, 5 * (height / sliderDistance));
+    SCHOOLMOVESPEED = schoolMoveSpeedSlider.value() / 100;
+    text("School pathing", 2 * (width / sliderDistance) - 10, height - 65);
+    schoolMoveSpeedSlider.position(2 * (width / sliderDistance) - 75, height - 60);
 
-   SCHOOLPOPULATION = schoolPopulation.value();
-   text("School population: " + SCHOOLPOPULATION, 20, 6.5 * (height / sliderDistance));
-   schoolPopulation.position(20, 6 * (height / sliderDistance));
+    FISHMOVESPEED = fishMoveSpeedSlider.value() / 100;
+    text("Fish pathing", 3 * (width / sliderDistance) - 10, height - 65);
+    fishMoveSpeedSlider.position(3 * (width / sliderDistance) - 75, height - 60);
+
+    FISHSPEEDLIMIT = fishSpeedLimitSlider.value();
+    text("Fish speed", 4 * (width / sliderDistance) - 10, height - 65);
+    fishSpeedLimitSlider.position(4 * (width / sliderDistance) - 75, height - 60);
+
+    FISHDENSITY = fishDensitySlider.value();
+    text("Fish density", 5 * (width / sliderDistance) - 10, height - 65);
+    fishDensitySlider.position(5 * (width / sliderDistance) - 75, height - 60);
+
+    SCHOOLPOPULATION = schoolPopulation.value();
+    text("School population", 6 * (width / sliderDistance) - 10, height - 65);
+    schoolPopulation.position(6 * (width / sliderDistance) - 75, height - 60);
 
 }
 
 function makeOcean() {
 
-   ocean = [];
-   foods = [];
-   addSchool();
-   addSchool();
-   addSchool();
-   //shark = new Shark(width / 2, height / 2, SHARKSIZE);
+    ocean = [];
+    foods = [];
+    addSchool();
+    addSchool();
+    addSchool();
+    //shark = new Shark(width / 2, height / 2, SHARKSIZE);
 
 }
 
 function addSchool() {
-   ocean.push(new School(-1, -1));
+    ocean.push(new School(-1, -1));
 }
 
 function addFood() {
-   foods.push(new Food(mouseX, mouseY));
+    foods.push(new Food(mouseX, mouseY));
 }
 
 function keyTyped() {
-   if (key == 's') {
-      console.log(ocean.length);
-      ocean.push(new School(mouseX, mouseY));
-   }
+    if (key == 's') {
+        console.log(ocean.length);
+        ocean.push(new School(mouseX, mouseY));
+    }
 }
 
 function mousePressed() {
-   if (mouseX > width / 5) {
-      addFood();
-   }
-
+    if (mouseY < height - 80 || mouseY > height - 20) {
+        addFood();
+    }
 }
 
 function windowResized() {
-   resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
